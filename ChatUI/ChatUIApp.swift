ccb79c5bc64839bc01b2843adece544f57b63867
nishantsethi <nsethi610@gmail.com>
2023-11-29 00:10:28 +0000

    Initial Commit

---
+++ ChatUI/ChatUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatUIApp.swift
+//  ChatUI
+//
+//  Created by Nishant sethi on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatUI
+//
+//  Created by Nishant sethi on 29/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
