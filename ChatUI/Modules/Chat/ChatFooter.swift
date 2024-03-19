041d22db7976887c77379db21be30728494fc6ae
nishantsethi <nsethi610@gmail.com>
2024-03-19 01:08:49 +0000

    modified code to hidden keyboard on sending message

--- ChatUI/Modules/Chat/ChatFooter.swift
+++ ChatUI/Modules/Chat/ChatFooter.swift
@@ -20,12 +20,13 @@ struct ChatFooter: View {
         VStack {
             HStack {
                 TextField("Ask Anything...", text: $message)
-                       .font(.custom("Satoshi", size: 16))
-                       .padding(10)
-                       .overlay(
-                           RoundedRectangle(cornerRadius: 14)
-                               .stroke(Color.secondary, lineWidth: 2))
-            .accessibilityLabel("Message input")
+                    .font(.custom("Satoshi", size: 16))
+                    .padding(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 14)
+                            .stroke(Color.secondary, lineWidth: 2))
+                    .focused($isInputActive)
+                    .accessibilityLabel("Message input")
                 Spacer()
                 Button(action: sendMessage) {
                     Image(systemName: "paperplane")
@@ -47,26 +48,23 @@ struct ChatFooter: View {
         }
         .onTapGesture {
             isInputActive = false
-//            dismissKeyboard()
+            //            dismissKeyboard()
         }
         
     }
     private func sendMessage() {
         promptModel.pushMessage(userMessage: message)
-         Task {
-             await promptModel.postMessageToGPT(prompt: prompt ?? "")
-         }
+        Task {
+            await promptModel.postMessageToGPT(prompt: prompt ?? "")
+        }
         message = ""
         isInputActive = false
     }
-    private func dismissKeyboard() {
-            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-        }
 }
 
 #Preview {
     ZStack {
-//        BackgroundGradient(from: Color(red: 0.42, green: 0.75, blue: 0.73), to: Color(red: 0, green: 0.15, blue: 0.33).opacity(0.8))
+        //        BackgroundGradient(from: Color(red: 0.42, green: 0.75, blue: 0.73), to: Color(red: 0, green: 0.15, blue: 0.33).opacity(0.8))
         ChatFooter(promptModel: PromptViewModel())
     }
 }
--- ChatUI/Modules/Home/Main.swift
+++ ChatUI/Modules/Home/Main.swift
@@ -14,31 +14,7 @@ struct Main: View {
     @ObservedObject var historyVM: HistoryViewModel
     var body: some View {
         NavigationStack {
-            //            Grid {
-            //                GridRow {
-            //                    MainHeader()
-            //                }
-            //                GridRow {
-            //                    NavigationLink(destination: Chat(promptVM: promptVM, historyVM: historyVM)) {
-            //                        NewChatSection()
-            //                    }
-            //                }
-            //                GridRow {
-            //                    withAnimation {
-            //                        ChatHistorySection(promptVM: promptVM, historyVM: historyVM)
-            //                    }
-            //                }
-            ////                Spacer()
-            //                GridRow {
-            //                    ExploreSection(promptVM: promptVM)
-            //                }
-            //                GridRow {
-            //                    PromptLibrarySection(promptVM: promptVM)
-            //                }
-            //            }
-            
             ZStack {
-                
                 TabView {
                     Group{
                         Chat(promptVM: promptVM, historyVM: historyVM).tabItem {
@@ -54,26 +30,8 @@ struct Main: View {
                             Label("Explore", systemImage: "globe")
                         }.tag(Tab.explore)
                     }
-                    
                 }
-//                                .frame(maxWidth:.infinity,maxHeight:.infinity)
                 .background(Color(red: 0.16, green: 0.18, blue: 0.20))
-//                VStack {
-//                    
-//                    HStack {
-//                        Spacer() // Pushes the button to the right
-//                        NavigationLink(destination: Chat(promptVM: promptVM, historyVM: historyVM)) {
-//                            Image(systemName: "plus.app")
-//                                .resizable()
-//                                .scaledToFit()
-//                                .frame(width: 40.0, height: 40.0) // Adjust the size as needed
-//                                .foregroundColor(.white)
-//                        }
-////                        .padding(.trailing, 20) // Adds padding on the right
-////                        .padding(28.0) // Adds padding at the bottom
-//                    }
-//                    Spacer() // Pushes the button to the bottom
-//                }
             }
             
         }
